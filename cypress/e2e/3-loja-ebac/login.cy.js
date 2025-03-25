187fa7cd4f7c757a9439113025ad3fd7656eca0c
Aires Jr <airessk8@gmail.com>
2025-03-25 22:42:18 +0000

    Testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,14 @@
 /// <reference types="cypress" />
 
 describe('Funcionalidade: Login', () => {
-
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('testeaires@ebac.com')
         cy.get('#password').type('teste1234@')
         cy.get('.woocommerce-form > .button').click()
@@ -11,5 +16,18 @@ describe('Funcionalidade: Login', () => {
 
     })
 
+    it('Deve fazer exibir uma mensagem de erro ao insrir usuário inválido', () => {
+        cy.get('#username').type('t123asdi@ebac.com')
+        cy.get('#password').type('teste1234@')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve exibir uma mensagem de error ao inserir senha inválida', () => {
+        cy.get('#username').type('testeaires@ebac.com')
+        cy.get('#password').type('teste1234')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('exist')
+    });
 
 })
